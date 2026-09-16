95d93a24cef8321f18596bb1aa90711a840f524e
sandipmandaldev <sandipmandal2610@gmail.com>
2026-09-16 15:41:24 +0530

    Fix CSRF trusted origin

--- CALL/settings.py
+++ CALL/settings.py
@@ -25,7 +25,15 @@ SECRET_KEY = 'django-insecure-ky4-*dr+0*5ir$mh$cl7w)$wvb3hj*g!9n_y9_29fk4l_da@%o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [
+    "ocall.onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://ocall.onrender.com",
+]
 
 
 # Application definition
